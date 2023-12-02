30bc609fa9fda383de74d267a86d6ecf2e782402
rscudder <rscudder@expedia.com>
2023-12-01 20:25:40 -0600

    AoC Day1

--- calendar/day01/Day1.kt
+++ calendar/day01/Day1.kt
@@ -4,11 +4,41 @@ import Day
 import Lines
 
 class Day1 : Day() {
+    val digits:List<String> = listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.fold(0) { total, line: String ->
+            val firstDigit = line.first { it.isDigit() }
+            val lastDigit = line.findLast { it.isDigit() }
+
+            "$firstDigit$lastDigit".toInt() + total
+        }
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.fold(0) { total, line: String ->
+            val firstDigit = line.findAnyOf(digits).let(::getDigit)
+            val lastDigit = line.findLastAnyOf(digits).let(::getDigit)
+
+            "$firstDigit$lastDigit".toInt() + total
+        }
     }
+
+    private fun getDigit(stringDigit:Pair<Int, String>?) : String {
+        return when(stringDigit!!.second) {
+            "zero" -> "0"
+            "one" -> "1"
+            "two" -> "2"
+            "three" -> "3"
+            "four" -> "4"
+            "five" -> "5"
+            "six" -> "6"
+            "seven" -> "7"
+            "eight" -> "8"
+            "nine" -> "9"
+            else -> stringDigit.second
+        }
+    }
+
 }
